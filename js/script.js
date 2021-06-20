7abe4c0c63cf7ecc40d240b07a0162c4f54e8176
durgeshlinearloop <durgesh@linearloop.io>
2021-06-20 19:26:12 +0530

    Listing done, add data done

--- js/script.js
+++ js/script.js
@@ -1,50 +1,9 @@
-showFormData();
+import { showFormData, takeInputFormData } from "./helper.js";
 
-var formData = [];
-let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function (e) {
-  console.log("HELLL");
-  let roomId = document.getElementById("roomId").value;
-  let roomType = document.getElementById("roomType").value;
-  let roomLocation = document.getElementById("roomLocation").value;
-  let roomCharge = document.getElementById("roomCharge").value;
+if (window.location.href === "http://127.0.0.1:5500/html/adminformdata.html") {
+  showFormData();
+}
 
-  myFormData = {
-    roomId,
-    roomType,
-    roomLocation,
-    roomCharge,
-  };
-
-  formData.push(myFormData);
-
-  localStorage.setItem("formData", JSON.stringify(formData));
-
-  roomId.value = "";
-  roomType.value = "";
-  roomLocation.value = "";
-  roomCharge.value = "";
-  // showFormData();
-});
-
-function showFormData() {
-  let formData = JSON.parse(localStorage.getItem("formData"));
-  console.log("SHOWFORMDATA ", formData);
-  let formValuesElm = document.getElementById("formValues");
-
-  if (formData?.length >= 1) {
-    let html = "";
-    formData.forEach(function (element) {
-      html += `<tr>
-               <td>${element.roomId}</td>
-               <td>${element.roomType}</td>
-               <td>${element.roomLocation}</td>
-               <td>${element.roomCharge}</td>
-              </tr>`;
-    });
-    formValuesElm.innerHTML = html;
-  } else {
-    let html = "Yoy don't have any data to show";
-    formValuesElm.innerHTML = html;
-  }
+if (window.location.href === "http://127.0.0.1:5500/html/createRoom.html") {
+  takeInputFormData();
 }
