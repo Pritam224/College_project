fb45b74af0b462dda3ff641bf227936c5953e6b0
durgeshlinearloop <durgesh@linearloop.io>
2021-06-20 22:18:37 +0530

    Delete functionality added

--- js/script.js
+++
@@ -1,9 +0,0 @@
-import { showFormData, takeInputFormData } from "./helper.js";
-
-if (window.location.href === "http://127.0.0.1:5500/html/adminformdata.html") {
-  showFormData();
-}
-
-if (window.location.href === "http://127.0.0.1:5500/html/createRoom.html") {
-  takeInputFormData();
-}
